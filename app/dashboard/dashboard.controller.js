a21a7d54ffc55ab9d6579cf86e6363f3215f9e62
k1631351 <k1631351@kcl.ac.uk>
2018-11-10 09:16:21 +0000

    Amother one..

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -1,23 +1,13 @@
 app.controller('DashboardCtrl', function(Dashboard, Auth, $state, $timeout, $q, $log) {
   var self = this;
 
-    self.simulateQuery = false;
-    self.isDisabled    = false;
-    self.questions = Dashboard.getArray();
-
-    self.states        = loadAll();
-    self.querySearch   = querySearch;
-    self.selectedItemChange = selectedItemChange;
-    self.searchTextChange   = searchTextChange;
+    self.questions = Dashboard.all;
 
 
     self.logout = function() {
-      self.profile.online = null;
-      self.profile.$save().then(function() {
         Auth.$signOut().then(function() {
           $state.go('home');
         });
-      });
     };
 
     self.newQuestion = {
@@ -37,48 +27,25 @@ app.controller('DashboardCtrl', function(Dashboard, Auth, $state, $timeout, $q, 
        self.availableUsers = self.users; // re-initialise the available users
    };
 
-   function addToArray () {
-     var array = []
-     for (var i = 0; i < self.questions.length; i++) {
-       arrray.push(self.questions[i])
-     }
-     return array
+
+
+    self.loadAll = function() {
+      var allQuestions = [];
+
+      for (var i = 0; i <   self.questions.length; i++) {
+        if (self.questions[i]) {
+          allQuestions.push(self.questions[i].Question);
+        }
+      }
+
+      return allQuestions.map(function(question) {
+        return question.toLowerCase();
+      });
     };
 
-    console.log(addToArray())
-
-    function querySearch (query) {
-      var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
-          deferred;
-      if (self.simulateQuery) {
-        deferred = $q.defer();
-        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-        return deferred.promise;
-      } else {
+    self.querySearch = function(query) {
+      var results = query ? self.loadAll().filter( createFilterFor(query) ) : self.loadAll()
         return results;
-      }
-    }
-
-    function searchTextChange(text) {
-      $log.info('Text changed to ' + text);
-    }
-
-    function selectedItemChange(item) {
-      $log.info('Item changed to ' + JSON.stringify(item));
-    }
-
-    /**
-     * Build `states` list of key/value pairs
-     */
-    function loadAll() {
-      var allStates = ""
-
-      return allStates.split(/, +/g).map( function (state) {
-        return {
-          value: state.toLowerCase(),
-          display: state
-        };
-      });
     }
 
     /**
